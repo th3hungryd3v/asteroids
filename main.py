de19755b985659867012f4b6123aa54075880fab
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-12 10:31:06 -0700

    fix .gitignore, create game loop, pygame.QUIT - this will make the button 'X' work to exit game

--- main.py
+++ main.py
@@ -4,10 +4,24 @@
 import pygame
 from constants import *
 
+
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                return
+                
+        screen.fill((0, 0, 0)) # (Red, Green, Blue)
+
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
