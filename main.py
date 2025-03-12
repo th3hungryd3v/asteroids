d83b71e832372b18870ffa1421cb57777d6ae778
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-12 10:50:26 -0700

    define clock object, initialize dt, tick at the end of each iteration of the game loop + update the value of dt

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock() # Clock Object
+    dt = 0 # Delta Time Variable Initialized
 
     running = True
     while running:
@@ -18,10 +20,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
                 return
-                
+
         screen.fill((0, 0, 0)) # (Red, Green, Blue)
 
     pygame.display.flip()
+    
+    dt = clock.tick(60) / 1000 # Tick at the end of each iteration, get delta time and update dt
 
 if __name__ == "__main__":
     main()
