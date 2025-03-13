f96d7880053fdd894e4333693937d0685c214fe6
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-13 08:30:51 -0700

    call the parent class's constructor and also passing in PLAYER RADIUS

--- player.py
+++ player.py
@@ -2,4 +2,6 @@ import pygame
 from constants import PLAYER_RADIUS
 
 class Player(CircleShape):
-    def __init__(self, x, y)
+    def __init__(self, x, y):
+        super().__init__(PLAYER_RADIUS)
+        pass
