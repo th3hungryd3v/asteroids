96943df8116f3669f3c8f55e2c06e53675db79e9
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-13 15:35:46 -0700

    Update the update method, add keys (w,s) for movement

--- player.py
+++ player.py
@@ -30,6 +30,12 @@ class Player(CircleShape):
 
         if keys[pygame.K_d]:
             self.rotate(dt)
-    
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
