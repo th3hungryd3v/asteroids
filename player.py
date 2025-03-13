f9cf087e99dec0a882b51fe2753705758ec70529
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-13 10:57:12 -0700

    import player turn speed, add a new method (rotate)

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
-from circleshape import *
-from constants import PLAYER_RADIUS
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
