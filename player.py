bd9c24f1aed75f3e8762aa98e3a904e849025fbe
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-13 08:38:35 -0700

    create a triangle method, but use a circle for its hitbox

--- player.py
+++ player.py
@@ -6,3 +6,11 @@ class Player(CircleShape):
         super().__init__(PLAYER_RADIUS)
         self.rotation = 0
         pass
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position + forward * self.radius - right
+        c = self.position + forward * self.radius + right
+        return [a, b, c]
