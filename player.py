f99c89fc1f46b488c9c46b6e842c02ef772104f6
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-13 10:50:51 -0700

    properly show the player (triangle)
    I used the wrong operators for the b & c variables, changing them from '+' to '-' now correctly shows the triangle (player)

--- player.py
+++ player.py
@@ -12,9 +12,9 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
-        b = self.position + forward * self.radius - right
-        c = self.position + forward * self.radius + right
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, "white", self.triangle(), 10)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
