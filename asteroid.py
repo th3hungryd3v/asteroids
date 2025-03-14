745ae45d3e60f48893a2a6df7b1060b63bed6e41
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-13 17:35:02 -0700

    Override the draw method

--- asteroid.py
+++ asteroid.py
@@ -4,4 +4,5 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
     
-    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "gray", (int(self.position.x), int(self.position.y), self.radius, 2))
