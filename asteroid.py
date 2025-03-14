e3a98d13eb55db3ca45fa512a4bc2ff9b7af8070
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-13 19:16:05 -0700

    Add a pair of parens for the overridden draw method, add dt parameter
    The game now works as expected -- spawning circles, triangular player able to move around the screen (w,a,s,d)

--- asteroid.py
+++ asteroid.py
@@ -1,11 +1,13 @@
+import pygame
 from circleshapes import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
     
     def draw(self, screen):
-        pygame.draw.circle(screen, "gray", (int(self.position.x), int(self.position.y), self.radius, 2))
+        pygame.draw.circle(screen, "gray", (int(self.position.x), int(self.position.y)), self.radius, 2)
 
-    def update(self):
+    def update(self, dt):
         self.position += self.velocity * dt
