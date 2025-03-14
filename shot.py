363df131d2e08d1db8b18b8695c203b9e199cb79
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-14 08:54:56 -0700

    Update draw and update methods, similar to asteroid.py

--- shot.py
+++ shot.py
@@ -5,3 +5,9 @@ from constants import SHOT_RADIUS
 class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), self.radius)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
