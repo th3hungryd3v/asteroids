393a8894a7657efc2036013eaab0cbb1905c1dac
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-14 08:49:18 -0700

    define Shot

--- shot.py
+++ shot.py
@@ -2,3 +2,6 @@ import pygame
 from circleshapes import CircleShape
 from constants import SHOT_RADIUS
 
+class Shot(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
