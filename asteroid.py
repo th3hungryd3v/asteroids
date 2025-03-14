52102739e7d37b612e58c3566cf46336a9add9bb
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-14 11:42:05 -0700

    import random + create 2 new asteroids for medium and large asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from circleshapes import CircleShape
 from constants import *
 
@@ -11,3 +12,16 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return # small asteroids -- no splitting
+        new_radius = self.radius / 2
+        random_angle = random.randint(20, 50)
+        for angle in [random_angle, -random_angle]:
+            new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            new_speed = random.randint(40, 100)
+            new_direction = pygame.Vector2(1, 0).rotate(angle)
+            new_asteroid_velocity = new_direction * new_speed
