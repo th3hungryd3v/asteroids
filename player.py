d786180030cba9aefdb065e36bf011b756090429
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-14 11:10:29 -0700

    corrected position, player's bullets should come from the front

--- player.py
+++ player.py
@@ -27,7 +27,7 @@ class Player(CircleShape):
     def shoot(self):
         if self.shot_timer <= 0: # shoot if the timer is greater than zero
             shot = Shot(self.position.x, self.position.y)
-            forward = pygame.Vector2(0, -1).rotate(self.rotation)
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
             shot.velocity = forward * PLAYER_SHOOT_SPEED
             self.shot_timer = PLAYER_SHOOT_COOLDOWN # reset the timer back to 0.03
 
